ca5184b4411afd0ffb0bb14b273d1a866d318c75
lefty14nsettings <r.h614@icloud.com>
2022-02-26 12:15:14 +0900

    commitテスト

--- Java_Basic/Q6.java
+++ Java_Basic/Q6.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 public class Q6 {
 	public static void main(String[] args) throws IOException {
-		// メッセージ
+		// メッセージ出力
 		System.out.println("文字を入力してください");
 		// BufferedReader生成（文字列を渡すため、数字を使いたい場合は変換する）
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
